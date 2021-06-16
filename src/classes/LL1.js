3784cd8d66fdbcef900561f7f0c76944c841015b
EduardoMtz1 <hbkeduardo10@hotmail.com>
2021-06-15 19:45:45 -0500

    First fixed, Follow created

--- src/classes/LL1.js
+++ src/classes/LL1.js
@@ -5,19 +5,22 @@ export default class LL1{
         this.terminals = terminals;
     }
 
-    first(ruleIn){
-        var aux = ruleIn.head;
+    first(ruleIn, i){
+        var aux = ruleIn[i];
         var firstSet = new Set();
-        if(this.terminals.has(aux.symbol)){
-            firstSet.add(aux.symbol);
+        if(this.terminals.has(aux)){
+            firstSet.add(aux);
         }else{
-            var au = aux.next;
-            if(this.terminals.has(au.symbol)){
-                firstSet.add(au.symbol); 
-            }
             this.rules.forEach(rule=>{
-                if(rule.head.symbol === au.symbol){
-                    firstSet = new Set([...firstSet, ...this.first(rule)]);
+                var ar = rule.toList();
+                if(ar[0] === aux){
+                    var au = ar[1];
+                    if(this.terminals.has(au)){
+                        firstSet.add(au); 
+                    }else{
+                        firstSet = new Set([...firstSet, ...this.first(ar,1)]);
+                    }
+                    
                 }
             })
         }
@@ -25,28 +28,42 @@ export default class LL1{
     }
 
     follow(simb){
-        console.log("Follow simbolo: " + simb);
+        //console.log("Follow simbolo: " + simb);
         var followSet = new Set();
         var i = 0;
+        var j = 0;
+        var simbIni;
         this.rules.forEach(rule =>{
-            var aux = rule.head.next;
-            while(aux!==null){
-                if(aux.symbol === simb){
-                    var  auxSet = new Set();
-                    auxSet = this.first(aux);
-                    if(i ===0){
-                        auxSet.add("$");
-                    }
-                    if(auxSet.has("EPSILON")){
-                        auxSet.delete("EPSILON");
-                        followSet = new Set([...followSet, ...this.follow(rule.head.symbol)]);
+            var aux = rule.toList();
+            if(i === 0 && aux[0] === simb){
+                simbIni = aux[0];
+            }           
+            for(j =1; j<aux.length; j++){
+                if(aux[j] === simb){
+                    var  auxSet = new Set();  
+                    if(j === aux.length-1){
+                        if(aux[0]!==simb)
+                        auxSet = new Set([...auxSet, ...this.follow(aux[0])]);
+                    }else{
+                        auxSet = new Set([...auxSet, ...this.first(aux, j+1)]);
+                        if(auxSet.has("EPSILON")){
+                            auxSet.delete("EPSILON");
+                            if(aux[0]!==simb)
+                            auxSet = new Set([...auxSet, ...this.follow(aux[0])]);
+                        }
                     }
                     followSet = new Set([...followSet, ...auxSet]);
                 }
-                aux = aux.next;
             }
-            i++;
-        })
+            i = i+1;
+        });
+        if(simbIni === simb){
+            followSet.add("$");
+        }else{
+            if(followSet.has("$")){
+                followSet.delete("$");
+            }
+        }
         return followSet;
     }
 }
